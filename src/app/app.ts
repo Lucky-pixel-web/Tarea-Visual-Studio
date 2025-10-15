1e89e285b7bb32b168c8de7994ab574c3a301421
Lucky-pixel-web <xj.109j.w@gmail.com>
2025-10-15 18:03:16 -0500

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostList } from './components/post-list/post-list';
+import { CommonModule } from '@angular/common';
+import { Users } from "./components/users/users";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PostList, CommonModule, Users],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
